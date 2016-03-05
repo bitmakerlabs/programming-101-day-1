e4510e81373718e3b2f4b6b3d4b4d4f29139be63
Fabio Gelsomini <fgelsomini@gmail.com>
2016-03-04 23:28:49 -0500

    Refactor to use vanilla JS

--- js/todos.js
+++ js/todos.js
@@ -1,30 +1,67 @@
-$(document).on('ready', function() {
+// Toggle the css class denoting completion when items are clicked
 
-  // Toggle css class denoting completion when items are clicked
-  $('.todo a').on('click', function() {
-    $(this).parent().toggleClass('is-complete');
+function updateCounter() {
+  var allTodos = document.querySelectorAll(".todo");
+  var completedTodos = document.querySelectorAll(".todo.is-complete");
+  document.getElementById("counter").innerHTML = allTodos.length - completedTodos.length;
+}
+
+updateCounter();
+
+var items = document.querySelectorAll(".todo a");
+for (var i=0; i < items.length; i++) {
+  items[i].addEventListener("click", function() {
+      this.parentNode.classList.toggle('is-complete');
+      updateCounter();
   });
+}
 
-  // Handle events for filter clicks
-  $('.todo-filter').on('click', function() {
-    $('.todo-filter').removeClass('is-active');
-  });
+// Handle events for filter clicks
 
-  $('.todo-filter.all').on('click', function() {
-    $('.todo').show();
-    $(this).addClass('is-active');
-  });
+function deactivateAllFilters() {
+  var filters = document.querySelectorAll(".todo-filter");
+  for (var i=0; i < filters.length; i++) {
+    filters[i].classList.remove('is-active');
+  }
+}
 
-  $('.todo-filter.remaining').on('click', function() {
-    $('.todo').show();
-    $('.todo.is-complete').hide();
-    $(this).addClass('is-active');    
-  });
+function showAllTodos() {
+  var todos = document.querySelectorAll(".todo");
+  for (var i=0; i < todos.length; i++) {
+    todos[i].style.display = 'block';
+  }
+}
 
-  $('.todo-filter.completed').on('click', function() {
-    $('.todo').hide();
-    $('.todo.is-complete').show();
-    $(this).addClass('is-active');    
-  });
+function hideAllTodos() {
+  var todos = document.querySelectorAll(".todo");
+  for (var i=0; i < todos.length; i++) {
+    todos[i].style.display = 'none';
+  }
+}
 
+document.getElementById("show-all").addEventListener("click", function() {
+  deactivateAllFilters();
+  showAllTodos();
+  this.classList.add('is-active');
+});
+
+
+document.getElementById("show-remaining").addEventListener("click", function() {
+  deactivateAllFilters();
+  showAllTodos();
+  var completedTodos = document.querySelectorAll(".todo.is-complete");
+  for (var i=0; i < completedTodos.length; i++) {
+    completedTodos[i].style.display = 'none';
+  }
+  this.classList.add('is-active');
+});
+
+document.getElementById("show-completed").addEventListener("click", function() {
+  deactivateAllFilters();
+  hideAllTodos();
+  var completedTodos = document.querySelectorAll(".todo.is-complete");
+  for (var i=0; i < completedTodos.length; i++) {
+    completedTodos[i].style.display = 'block';
+  }
+  this.classList.add('is-active');
 });
